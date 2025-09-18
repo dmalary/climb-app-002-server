4b8f71a9ff5ef355c5f10ce30883c9824105aca4
madhat5 <davidmalary@gmail.com>
2025-09-18 16:43:19 -0400

    feat: update attempt db calls

--- controllers/attemptController.js
+++ controllers/attemptController.js
@@ -7,7 +7,27 @@ export const getAllAttempts = async (req, res) => {
 
     if (error) throw error;
 
-    console.log("data", data);
+    console.log("data", data.length > 1);
+    return res.status(200).json(data);
+  } catch (err) {
+    console.error("Error fetching users:", err.message);
+    return res.status(500).json({ error: err.message });
+  }
+};
+
+export const getUserSessionAttempts = async (req, res) => {
+  const { sessionId } = req.params;
+
+  try {
+    if (!sessionId) {
+      return res.status(400).json({ error: "Missing sessionId" });
+    }
+
+    const { data, error } = await supabase.from("attempts").select('*').eq('session_id', sessionId);
+
+    if (error) throw error;
+
+    console.log("data", data.length > 1);
     return res.status(200).json(data);
   } catch (err) {
     console.error("Error fetching users:", err.message);
--- routes/attemptRoutes.js
+++ routes/attemptRoutes.js
@@ -1,8 +1,9 @@
 import express from'express';
-import { getAllAttempts } from '../controllers/attemptController.js'
+import { getAllAttempts, getUserSessionAttempts } from '../controllers/attemptController.js'
 
 const router = express.Router();
 
-router.get('/', getAllAttempts)
+router.get('/', getAllAttempts);
+router.get('/:sessionId', getUserSessionAttempts)
 
 export default router;
