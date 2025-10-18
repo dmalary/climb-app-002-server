f6efc4788a0d964a6f82b195ef7c0594a185f5fa
madhat5 <davidmalary@gmail.com>
2025-10-18 11:42:52 -0400

    feat: update syncUser. next to supa flow

--- config/middleware/auth.js
+++ config/middleware/auth.js
@@ -1,22 +1,28 @@
-import { clerkMiddleware, requireAuth } from '@clerk/express';
 import { supabase } from "../supabaseClient.js";
 
 export const syncUser = async (req, res, next) => {
   try {
-    const { userId, sessionId } = req.auth;
+    // const { userId, sessionId } = req.auth;
+    const { userId, sessionClaims } = req.auth;
+    // console.log('userId', userId)
 
     if (!userId) {
       return res.status (401).json({ error: "Unauthorized, no user id found" });
     }
 
+    // Basic user data from Clerk
+    const email = sessionClaims?.email || "";
+    const username = sessionClaims?.username || "";
+
     // check supabase for user
     let { data: existingUser, error: findErr } = await supabase
       .from("users")
       .select("*")
-      .eq("clerk_id", userId)
+      // .eq("clerk_id", userId)
+      .eq("id", userId)
       .single();
 
-
+    // Ignore "no rows" error
     if (findErr && findErr.code !== "PGRST116") {
       console.error("Supabase find user error:", findErr);
       return res.status(500).json({ error: "Internal error" });
@@ -25,7 +31,14 @@ export const syncUser = async (req, res, next) => {
     if (!existingUser) {
       const { data: newUser, error: insertErr } = await supabase
         .from("users")
-        .insert({ clerk_id: userId })
+        // .insert({ clerk_id: userId })
+        // .insert({ id: userId })
+        .insert({
+          id: userId,
+          email,
+          username,
+          created_at: new Date().toISOString()
+        })
         .select("*")
         .single();
 
