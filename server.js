f696ae2857412a0752d8f9847225abc6e368f048
madhat5 <davidmalary@gmail.com>
2025-10-27 13:44:27 -0400

    fix: update url call

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import { syncUser } from './config/middleware/auth.js';
 const app = express();
 
 // app.use(cors());
-app.use(cors({ origin: process.env.PORT, credentials: true }));
+app.use(cors({ origin: process.env.NEXT_URL, credentials: true }));
 app.use(express.json());
 app.use(clerkMiddleware());
 
