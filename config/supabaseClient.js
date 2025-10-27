7d3ed542edb8d2b097f359b62b54a27a78eebc27
madhat5 <davidmalary@gmail.com>
2025-10-27 13:44:52 -0400

    fix: update url call

--- config/supabaseClient.js
+++ config/supabaseClient.js
@@ -3,8 +3,8 @@ import { createClient } from "@supabase/supabase-js";
 
 dotenv.config()
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-// const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.PUBLIC_SUPABASE_URL;
+// const supabaseKey = process.env.PUBLIC_SUPABASE_ANON_KEY;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 // export const supabase = createClient(supabaseUrl, supabaseKey);
